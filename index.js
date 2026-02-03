d10b3eb0a213a1abffc4982586a200f41f48720e
Souvik Maity <souvik.orbitup@gmail.com>
2026-02-03 17:47:06 +0530

    fix(cors): update CORS origin to use FRONTEND_URL environment variable

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
